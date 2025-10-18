cb0380d0a4dc79477675aa36a4214af3b56d44db
Liz <lizandrotaku@gmail.com>
2025-10-18 15:41:32 -0500

    KGHJHCF

--- CAJERO/PRODUCTO.cs
+++ CAJERO/PRODUCTO.cs
@@ -8,5 +8,22 @@ namespace CAJERO
 {
     public class PRODUCTO
     {
+        //ATRIBUTOS PARA PRODUCTOS
+        public string NOMBRE;
+        public double PRECIO;
+
+        //CONSTRUCTOR PARA PRODUCTOS
+        public PRODUCTO(string NOMBRE, double PRECIO)
+        {
+            this.NOMBRE = NOMBRE;
+            this.PRECIO = PRECIO;
+        }
+
+        //METODO PARA MOSTRAR INFORMACION DE PRODUCTOS
+        public string MostrarProducto()
+        {
+            return "NOMBRE: " + NOMBRE + " PRECIO: " + PRECIO;
+        }
+
     }
 }
